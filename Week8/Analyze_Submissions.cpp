f7e2107519b7cd0cfd1f3afed6fe6c241dee95f9
Hung <hungnguyentrong285@gmail.com>
2023-12-14 21:48:15 +0700

    Tuan8

---
+++ Week8/Analyze_Submissions.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int number,er_sub;
+
+map<string, int> doanhthucuahang;
+map<pair<string, string>, int> cuahangdoanhthu;
+
+int money_time[86401];
+int sotiendenkhi[86400];
+
+int main(){
+    
+}
